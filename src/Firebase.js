a0ab31e84080c2e2a0ba28c55ebfa16e1f0d917f
soulden1202 <74317980+soulden1202@users.noreply.github.com>
2022-05-23 00:09:12 -0400

    firebase

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,13 +1,16 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 
+
+
+//firebase config
 export const auth = firebase
   .initializeApp({
-    apiKey: "AIzaSyAxiso1jY77ancsstCEcFnh2QHKamqhef0",
-    authDomain: "unichat-cf076.firebaseapp.com",
-    projectId: "unichat-cf076",
-    storageBucket: "unichat-cf076.appspot.com",
-    messagingSenderId: "388566106172",
-    appId: "1:388566106172:web:aea0ade7c661aaa0694339",
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
   })
   .auth();
